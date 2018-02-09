a5ab831d20591688ef306d6a0ae0e9e936e1b24a
Sharon Olorunniwo <olorunns@tcd.ie>
2018-02-09 18:46:13 +0000

    Add files via upload

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -1,54 +1,146 @@
 package engineTester;
 
-import Shaders.StaticShader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import models.RawModel;
+import models.TexturedModel;
+
 import org.lwjgl.opengl.Display;
+import org.lwjgl.util.vector.Vector2f;
+import org.lwjgl.util.vector.Vector3f;
+
 import renderEngine.DisplayManager;
 import renderEngine.Loader;
-import renderEngine.RawModel;
-import renderEngine.Renderer;
+import renderEngine.MasterRenderer;
+import renderEngine.OBJLoader;
+import terrains.Terrain;
+import textures.ModelTexture;
+import textures.TerrainTexture;
+import textures.TerrainTexturePack;
+import entities.Camera;
+import entities.Entity;
+import entities.Light;
+import entities.Player;
+import guis.GuiRenderer;
+import guis.GuiTexture;
 
 public class MainGameLoop {
 
-    public static  void main(String[] args){
+	public static void main(String[] args) {
 
-        DisplayManager.createDisplay();
-        Loader loader = new Loader();
-        Renderer renderer = new Renderer();
-        StaticShader shader = new StaticShader();
+		DisplayManager.createDisplay();
+		Loader loader = new Loader();
 
-        float[] vertices = {
-                -0.5f, 0.5f, 0,
-                -0.5f, -0.5f, 0,
-                0.5f, -0.5f, 0,
-                0.5f, 0.5f, 0
-        };
+		// *********TERRAIN TEXTURE STUFF***********
 
-        int[] indices = {
+		TerrainTexture backgroundTexture = new TerrainTexture(loader.loadTexture("grassy"));
+		TerrainTexture rTexture = new TerrainTexture(loader.loadTexture("dirt"));
+		TerrainTexture gTexture = new TerrainTexture(loader.loadTexture("pinkFlowers"));
+		TerrainTexture bTexture = new TerrainTexture(loader.loadTexture("path"));
 
-                0,1,3,  //Top Left Triangle(V0,V1,V3)
-                3,1,2   //bOTTOM RIGHT TRIANGLE (v3,v1,v2)
-        };
+		TerrainTexturePack texturePack = new TerrainTexturePack(backgroundTexture, rTexture, gTexture, bTexture);
+		TerrainTexture blendMap = new TerrainTexture(loader.loadTexture("blendMap"));
 
-        RawModel model = loader.loadToVAO(vertices, indices);
+		Terrain terrain = new Terrain(0, -1, loader, texturePack, blendMap, "heightmap");
 
-        while(!Display.isCloseRequested()){
+		// *****************************************
 
-            //game logic
+		RawModel model = OBJLoader.loadObjModel("tree", loader);
 
-            renderer.prepare();
-            shader.start();
-            renderer.render(model);
-            shader.stop();
-            DisplayManager.updateDisplay();
-        }
+		TexturedModel tree = new TexturedModel(model, new ModelTexture(loader.loadTexture("tree")));
+		TexturedModel grass = new TexturedModel(OBJLoader.loadObjModel("grassModel", loader), new ModelTexture(loader.loadTexture("grassTexture")));
+		TexturedModel flower = new TexturedModel(OBJLoader.loadObjModel("grassModel", loader), new ModelTexture(loader.loadTexture("flower")));
 
-        shader.cleanUp();
-        loader.cleanUp();
-        DisplayManager.closeDisplay();
+		ModelTexture fernTexture = new ModelTexture(loader.loadTexture("fern"));
+		fernTexture.setNumberOfRows(2);
+		TexturedModel fern = new TexturedModel(OBJLoader.loadObjModel("fern", loader), fernTexture);
 
-    }
+		TexturedModel bobble = new TexturedModel(OBJLoader.loadObjModel("lowPolyTree", loader), new ModelTexture(loader.loadTexture("lowPolyTree")));
+		TexturedModel lamp = new TexturedModel(OBJLoader.loadObjModel("lamp", loader), new ModelTexture(loader.loadTexture("lamp")));
+
+		grass.getTexture().setHasTransparency(true);
+		grass.getTexture().setUseFakeLighting(true);
+		flower.getTexture().setHasTransparency(true);
+		flower.getTexture().setUseFakeLighting(true);
+		fern.getTexture().setHasTransparency(true);
+
+		List<Entity> entities = new ArrayList<Entity>();
+		Random random = new Random();
+		for (int i = 0; i < 500; i++) {
+			if (i % 7 == 0) {
+				float x = random.nextFloat() * 800 - 400;
+				float z = random.nextFloat() * -600;
+				float y = terrain.getHeightOfTerrain(x, z);
+				entities.add(new Entity(fern, random.nextInt(4), new Vector3f(x, y, z), 0, random.nextFloat() * 360, 0, 0.9f));
+
+				x = random.nextFloat() * 800 - 400;
+				z = random.nextFloat() * -600;
+				y = terrain.getHeightOfTerrain(x, z);
+				entities.add(new Entity(grass, new Vector3f(x, y, z), 0, 0, 0, 1.8f));
+
+				x = random.nextFloat() * 800 - 400;
+				z = random.nextFloat() * -600;
+				y = terrain.getHeightOfTerrain(x, z);
+				entities.add(new Entity(flower, new Vector3f(x, y, z), 0, 0, 0, 2.3f));
+			}
+
+			if (i % 3 == 0) {
+				float x = random.nextFloat() * 800 - 400;
+				float z = random.nextFloat() * -600;
+				float y = terrain.getHeightOfTerrain(x, z);
+				entities.add(new Entity(bobble, new Vector3f(x, y, z), 0, random.nextFloat() * 360, 0, random.nextFloat() * 0.1f + 0.6f));
+
+				x = random.nextFloat() * 800 - 400;
+				z = random.nextFloat() * -600;
+				y = terrain.getHeightOfTerrain(x, z);
+				entities.add(new Entity(tree, new Vector3f(x, y, z), 0, 0, 0, random.nextFloat() * 1 + 4));
+			}
+
+		}
+
+		List<Light> lights = new ArrayList<Light>();
+		lights.add(new Light(new Vector3f(0, 1000, -7000), new Vector3f(0.4f, 0.4f, 0.4f)));
+		lights.add(new Light(new Vector3f(185, 10, -293), new Vector3f(2, 0, 0), new Vector3f(1, 0.01f, 0.002f)));
+		lights.add(new Light(new Vector3f(370, 17, -300), new Vector3f(0, 2, 2), new Vector3f(1, 0.01f, 0.002f)));
+		lights.add(new Light(new Vector3f(293, 7, -305), new Vector3f(2, 2, 0), new Vector3f(1, 0.01f, 0.002f)));
+
+		entities.add(new Entity(lamp, new Vector3f(185, -4.7f, -293), 0, 0, 0, 1));
+		entities.add(new Entity(lamp, new Vector3f(370, 4.2f, -300), 0, 0, 0, 1));
+		entities.add(new Entity(lamp, new Vector3f(293, -6.8f, -305), 0, 0, 0, 1));
+
+		MasterRenderer renderer = new MasterRenderer();
+
+		RawModel playerModel = OBJLoader.loadObjModel("player", loader);
+		TexturedModel playerTexturedModel = new TexturedModel(playerModel, new ModelTexture(loader.loadTexture("playerTexture")));
+
+		Player player = new Player(playerTexturedModel, new Vector3f(100, 5, -150), 0, 180, 0, 0.6f);
+		Camera camera = new Camera(player);
+
+		List<GuiTexture> guis = new ArrayList<GuiTexture>();
+		GuiTexture health = new GuiTexture(loader.loadTexture("gui/health"), new Vector2f(-0.74f, 0.925f), new Vector2f(0.25f, 0.25f));
+		guis.add(health);
+
+		GuiRenderer guiRenderer = new GuiRenderer(loader);
+
+		while (!Display.isCloseRequested()) {
+			player.move(terrain);
+			camera.move();
+			renderer.processEntity(player);
+			renderer.processTerrain(terrain);
+			for (Entity entity : entities) {
+				renderer.processEntity(entity);
+			}
+			renderer.render(lights, camera);
+			guiRenderer.render(guis);
+			DisplayManager.updateDisplay();
+		}
+
+		guiRenderer.cleanUp();
+		renderer.cleanUp();
+		loader.cleanUp();
+		DisplayManager.closeDisplay();
+
+	}
 }
-
-
-
-
--- src/renderEngine/DisplayManager.java
+++ src/renderEngine/DisplayManager.java
@@ -1,37 +1,58 @@
 package renderEngine;
+
 import org.lwjgl.LWJGLException;
-import org.lwjgl.opengl.*;
+import org.lwjgl.Sys;
+import org.lwjgl.opengl.ContextAttribs;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.PixelFormat;
 
 public class DisplayManager {
 
-    private static  final  int WIDTH = 1280;
-    private static final  int HEIGHT = 720;
-    private static final  int FPS_CAP = 120;
+	private static final int WIDTH = 1280;
+	private static final int HEIGHT = 720;
+	private static final int FPS_CAP = 120;
 
-    public static void createDisplay (){
+	private static final String TITLE = "3D Game Engine - ThinMatrix tutorial";
 
-        ContextAttribs attribs = new ContextAttribs(3,2);
-        attribs.withForwardCompatible(true);
-        attribs.withProfileCore(true);
+	private static long lastFrameTime;
+	private static float delta;
 
-        try {
-            Display.setDisplayMode(new DisplayMode(WIDTH,HEIGHT));
-            Display.create(new PixelFormat(), attribs);
-            Display.setTitle("Our First Display!");
-        } catch (LWJGLException e) {
-            e.printStackTrace();
-        }
+	public static void createDisplay() {
 
-        GL11.glViewport(0,0,WIDTH,HEIGHT);
-    }
+		ContextAttribs attribs = new ContextAttribs(3, 2).withForwardCompatible(true).withProfileCore(true);
 
-    public static void updateDisplay (){
-        Display.sync(FPS_CAP);
-        Display.update();
-    }
+		try {
+			Display.setDisplayMode(new DisplayMode(WIDTH, HEIGHT));
+			Display.create(new PixelFormat(), attribs);
+			Display.setTitle(TITLE);
+		} catch (LWJGLException e) {
+			e.printStackTrace();
+		}
 
-    public static  void closeDisplay (){
+		GL11.glViewport(0, 0, WIDTH, HEIGHT);
+		lastFrameTime = getCurrentTime();
+	}
 
-        Display.destroy();
-    }
+	public static void updateDisplay() {
+		Display.sync(FPS_CAP);
+		Display.update();
+		long currentFrameTime = getCurrentTime();
+		delta = (currentFrameTime - lastFrameTime) / 1000f;
+		lastFrameTime = currentFrameTime;
+	}
+
+	public static float getFrameTimeSeconds() {
+		return delta;
+	}
+
+	public static void closeDisplay() {
+		Display.destroy();
+	}
+
+	private static long getCurrentTime() {
+		return Sys.getTime() * 1000 / Sys.getTimerResolution();
+	}
+
 }
